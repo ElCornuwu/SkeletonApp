3a24d3b0ecb666f3d356c7c59dbfd5b93bed24b8
vi.cornou <133885035+ElCornuwu@users.noreply.github.com>
2024-10-20 19:48:06 -0300

    busqueda

--- src/app/home/administrar-viaje/administrar-viaje.component.ts
+++ src/app/home/administrar-viaje/administrar-viaje.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/datos.service';
+import { ModalController } from '@ionic/angular';
+import { FormularioViajeComponent } from 'src/app/formulario-viaje/formulario-viaje.component';
 
 @Component({
   selector: 'app-administrar-viaje',
@@ -7,8 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdministrarViajeComponent  implements OnInit {
 
-  constructor() { }
+  viajes: any[] = [];
+  userID: number | null = null;
 
-  ngOnInit() {}
+  constructor(private datosService: DatosService, private modalCtrl: ModalController) { } // Inyecta ModalController
+
+  ngOnInit() {
+    this.loadViajes();
+    this.userID = this.datosService.getUserNombre()?.id;
+  }
+
+  loadViajes() {
+    this.datosService.getViajes().subscribe((data) => {
+      console.log(data);
+      this.viajes = data.filter((viaje: any) => viaje.userID === this.userID);
+    }, (error) => {
+      console.error("Error al obtener los viajes: ", error);
+    });
+  }
+
+  eliminarViaje(id: number) {
+    this.datosService.deleteViaje(id).subscribe(() => {
+      console.log(`Viaje con ID ${id} eliminado`);
+      this.loadViajes();
+    }, (error) => {
+      console.error('Error al eliminar el viaje: ', error);
+    })
+  }
+
+  async mostrarFormularioNuevoViaje() {
+    const modal = await this.modalCtrl.create({
+      component: FormularioViajeComponent,
+    });
+
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+    if (data && data.viaje) {
+      data.viaje.userID = this.userID; // Asignar el userID al nuevo viaje
+      this.datosService.addViaje(data.viaje).subscribe(() => {
+        console.log('Nuevo viaje agregado');
+        this.loadViajes(); // Refresca la lista de viajes
+      }, (error) => {
+        console.error('Error al agregar el viaje: ', error);
+      });
+    }
+  }
+
+  async mostrarFormularioEditarViaje(viaje: any) {
+    const modal = await this.modalCtrl.create({
+      component: FormularioViajeComponent,
+      componentProps: { viaje }
+    });
+
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+    if (data && data.viaje) {
+      data.viaje.userID = this.userID; // Asegúrate de mantener el userID al editar
+      this.datosService.addViaje(data.viaje).subscribe(() => {
+        console.log('Viaje actualizado');
+        this.loadViajes(); // Refresca la lista de viajes
+      }, (error) => {
+        console.error('Error al actualizar el viaje: ', error);
+      });
+    }
+  }
 
 }
